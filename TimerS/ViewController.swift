46ecb685b7798db688f6449d84b2d5ab871df203
DayeonJung <ekdus0227@gmail.com>
2020-03-01 16:16:53 +0900

    기본 세팅 및 .gitignore파일 생성

--- TimerS/ViewController.swift
+++ TimerS/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import WebKit
+import PureLayout
 
 class ViewController: UIViewController {
 
+    var webView:WKWebView!
+
+    let urlString: String = "https://sandykim-code.github.io/sandy-timer/"
+    
+    var indicator: UIActivityIndicatorView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        self.webView = WKWebView()
+        self.view.addSubview(self.webView)
+        self.webView.autoPinEdgesToSuperviewSafeArea()
 
+        self.webView.navigationDelegate = self
+
+        self.webView.load(URLRequest(url: URL(string: self.urlString)!))
+
+        self.indicator = UIActivityIndicatorView(forAutoLayout: ())
+        self.view.addSubview(self.indicator!)
+        self.indicator?.autoCenterInSuperview()
+        
+    }
 
 }
 
+extension ViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.indicator?.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.indicator?.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        
+        let alert = UIAlertController(title: "네트워크 오류", message: error.localizedDescription, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "확인", style: .default, handler : nil)
+
+        alert.addAction(defaultAction)
+        self.present(alert, animated: false, completion: nil)
+
+    }
+    
+}
